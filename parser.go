b828eabd50002472b12abe3e82d4a1196b18a752
Sergey Vilgelm <sergey@vilgelm.com>
2021-04-27 22:14:40 -0500

    The Code

---
+++ parser.go
@@ -0,0 +1,23 @@
+package confjson
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
+
+// Parser is a conf.ParseFunc to parse the given json
+func Parser(_ context.Context, r io.Reader) (interface{}, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var res interface{}
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
